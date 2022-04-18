535336bbdd5d63550700341e716c36931c9f1d84
Sparkchaser13 <jvansan@student.wgu.edu>
2022-04-18 11:05:20 -0600

    Main Screen change

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/loginScreen.fxml"));
-        primaryStage.setTitle("Login Screen");
+        primaryStage.setTitle("Main Screen");
         primaryStage.setScene(new Scene(root, 400, 250));
         primaryStage.show();
         primaryStage.setOnCloseRequest(null);
