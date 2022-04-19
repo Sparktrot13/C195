bfef16caadfb5108fac3d4cd3fdc84f05f93529b
Sparkchaser13 <jvansan@student.wgu.edu>
2022-04-18 22:05:23 -0700

    added customer.fxml and controller

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,4 +1,8 @@
 package database;
+import java.sql.Connection;
 
 public class DBConnection {
+    private static final String protocol = "jdbc";
+    private static final String vendorName = "mysql:";
+    private static final String ipAddress = "//wgudb.ucertify.com:3306/";
 }
