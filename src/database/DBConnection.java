080385a4d5fa04ba30f451f17fa5e4687ee17d22
Sparkchaser13 <jvansan@student.wgu.edu>
2022-04-19 10:26:48 -0700

    working on dbConn

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,8 +1,37 @@
 package database;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
     private static final String protocol = "jdbc";
     private static final String vendorName = "mysql:";
     private static final String ipAddress = "//wgudb.ucertify.com:3306/";
+    private static final String dbName = "WJ06YG8";
+
+    private static final String jdbcURL = protocol + vendorName + ipAddress + dbName;
+
+    private static final String MYSQLJDBCDriver = "com.mysql.jdbc,Driver";
+    private static final String username = "U06YG0";
+    private static Connection conn = null;
+
+    public static Connection startConnection(){
+        try{
+            Class.forName(MYSQLJDBCDriver);
+            conn = DriverManager.getConnection(jdbcURL,username,Password.getPassword());
+
+            System.out.println("Connection Successful");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }return conn;
+    }
+    public static void closeConnection(){
+        try{
+            conn.close();
+        } catch (Exception e){
+            //do nothing
+        }
+    }
 }
