c3543efa63d189d21368db3d2e49f1f5a08bb28f
Sparkchaser13 <jvansan@student.wgu.edu>
2022-05-10 09:30:17 -0700

    language settings

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,27 +1,26 @@
 package main;
 
-import database.DBConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.time.ZoneId;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import static database.DBConnection.*;
 import static utility.Locales.*;
-import static utility.Utility.*;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) {
         try {
             Locale.setDefault(new Locale("en"));
             ResourceBundle rb = bundle();
-            Parent root = FXMLLoader.load(getClass().getResource("/view/loginScreen.fxml"), rb);
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/loginScreen.fxml")), rb);
             primaryStage.setTitle(rb.getString("screenTitle"));
             primaryStage.setScene(new Scene(root, 400, 250));
             primaryStage.show();
@@ -31,14 +30,12 @@ public class Main extends Application {
         }
     }
     public static void main(String[] args){
-        //Locale.setDefault(new Locale("fr"));
-        //ResourceBundle rb = bundle();
         if (Locale.getDefault().getLanguage().equals("fr")) {
             System.out.println("fr");
-            //System.out.println(rb.getString("username") + rb.getString("password"));
         } else {
             System.out.println("other");
         }
+        ZoneId.getAvailableZoneIds().forEach(System.out::println);
         startConnection();
         launch(args);
         closeConnection();
