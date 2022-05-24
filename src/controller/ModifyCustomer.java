51ae9c29038f2470d369db0cd56bf425177b7899
Sparkchaser13 <jvansan@student.wgu.edu>
2022-05-24 15:11:00 -0800

    language settings

--- src/controller/ModifyCustomer.java
+++ src/controller/ModifyCustomer.java
@@ -8,11 +8,9 @@ import model.Appointments;
 import model.Countries;
 import model.Customers;
 import model.FLDivisions;
-
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-
 import static database.DBAppointments.deleteAppt;
 import static model.Lists.*;
 import static utility.Utility.*;
@@ -20,8 +18,8 @@ import static utility.Validator.*;
 import static database.DBCustomers.*;
 
 public class ModifyCustomer {
-    public ComboBox Country_Combo;
-    public ComboBox Region_Combo;
+    public ComboBox<Countries> Country_Combo;
+    public ComboBox<FLDivisions> Region_Combo;
     public Label Country_Label;
     public Label Region_Label;
     public Label Postal_Label;
@@ -35,12 +33,12 @@ public class ModifyCustomer {
     public TextField Address_Cust;
     public TextField Postal_Cust;
     public Button Update_Cust;
-    public TableView ApptTable_Cust;
-    public TableColumn ID_Col;
-    public TableColumn Title_Col;
-    public TableColumn Location_Col;
-    public TableColumn Start_Col;
-    public TableColumn End_Col;
+    public TableView<Appointments> ApptTable_Cust;
+    public TableColumn<Object, Object> ID_Col;
+    public TableColumn<Object, Object> Title_Col;
+    public TableColumn<Object, Object> Location_Col;
+    public TableColumn<Object, Object> Start_Col;
+    public TableColumn<Object, Object> End_Col;
     public Label TableTitle_Label;
     public Label Title_Label;
     public Button Create_Appt;
@@ -48,14 +46,14 @@ public class ModifyCustomer {
     public Button Delete_Appt;
     public Button Back_Screen;
 
-    public void CountryCombo(ActionEvent actionEvent) {
-        Countries c = (Countries) Country_Combo.getSelectionModel().getSelectedItem();
+    public void CountryCombo() {
+        Countries c = Country_Combo.getSelectionModel().getSelectedItem();
         int country_ID = c.getCountry_ID();
         Region_Combo.setItems(searchCountries(country_ID));
         Region_Combo.getSelectionModel().selectFirst();
     }
 
-    public void UpdateCust(ActionEvent actionEvent) {
+    public void UpdateCust() {
         try{
             chkCustomerBlank(Name_Cust,Address_Cust,Phone_Cust,Postal_Cust,Region_Combo,Country_Combo);
             int ID = Integer.parseInt(ID_Cust.getText());
@@ -65,30 +63,27 @@ public class ModifyCustomer {
             String phone = Phone_Cust.getText().trim();
             Timestamp updated = Timestamp.valueOf(LocalDateTime.now());
             String updater = currentUser.getUser_Name();
-            int regID = ((FLDivisions) Region_Combo.getSelectionModel().getSelectedItem()).getDiv_ID();
+            int regID = (Region_Combo.getSelectionModel().getSelectedItem()).getDiv_ID();
             updateCustomer(name,address,phone, postal,updated,updater,regID,ID);
         } catch (NumberFormatException e){
-            System.out.println(e);
             alert(alertType.error,errorsFound.concat(errors.toString()),"Error");
         }
     }
 
-    public void CreateAppt(ActionEvent actionEvent) throws IOException {
+    public void CreateAppt() throws IOException {
         int chkID = Integer.parseInt(ID_Cust.getText().trim());
         int index = lookupCustomer.find(chkID);
         sendCustAppt(index);
-        ObservableList l = lookupAppts(chkID);
+        ObservableList<Appointments> l = lookupAppts(chkID);
         ApptTable_Cust.setItems(l);
     }
 
     public void UpdateAppt() throws IOException {
-        ///getLastURL = updateCustomerURL;
-        //getLastTitle = customerTitle;
         sendApptCust(ApptTable_Cust,Integer.parseInt(ID_Cust.getText()));
     }
 
-    public void DeleteAppt(ActionEvent actionEvent) {
-        Appointments a = (Appointments) ApptTable_Cust.getSelectionModel().getSelectedItem();
+    public void DeleteAppt() {
+        Appointments a = ApptTable_Cust.getSelectionModel().getSelectedItem();
         if (a == null){
             alert(alertType.error,selectionError, "Selection Error");
         } else {
@@ -97,9 +92,8 @@ public class ModifyCustomer {
         }
     }
 
-    public void BackScreen(ActionEvent actionEvent) throws IOException {
+    public void BackScreen(ActionEvent actionEvent) {
         closeScreen(actionEvent);
-        // viewScreen(actionEvent,getLastURL,getLastTitle);
     }
 
     public void populateCustomer(Customers customer) {
@@ -118,7 +112,7 @@ public class ModifyCustomer {
         System.out.println("Region code is: " + rc1);
 
         int custID = Integer.parseInt(ID_Cust.getText().trim());
-        ObservableList l = lookupAppts(custID);
+        ObservableList<Appointments> l = lookupAppts(custID);
         ApptTable_Cust.setItems(l);
         ID_Col.setCellValueFactory(new PropertyValueFactory<>("Appointment_ID"));
         Title_Col.setCellValueFactory(new PropertyValueFactory<>("Appt_Title"));
